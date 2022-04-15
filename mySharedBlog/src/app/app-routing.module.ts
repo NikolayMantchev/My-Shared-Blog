d413b326200aa6bd4295cb77a37d600d8a6a3c3e
NikolayMantchev <62190441+NikolayMantchev@users.noreply.github.com>
2022-04-16 00:37:10 +0200

    Route Guard

--- mySharedBlog/src/app/app-routing.module.ts
+++ mySharedBlog/src/app/app-routing.module.ts
@@ -1,57 +1,58 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { SignInComponent } from './pages/sign-in/sign-in.component';
-import { SignUpComponent } from './pages/sign-up/sign-up.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { PostsComponent } from './pages/posts/posts.component';
-import {CardComponent} from './pages/posts/card/card.component'
-import {CreatePostComponent} from './pages/posts/create-post/create-post.component'
-import { EditPostComponent } from './pages/posts/edit-post/edit-post.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
+import { SignInComponent } from "./pages/sign-in/sign-in.component";
+import { SignUpComponent } from "./pages/sign-up/sign-up.component";
+import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.component";
+import { PostsComponent } from "./pages/posts/posts.component";
+import { CardComponent } from "./pages/posts/card/card.component";
+import { CreatePostComponent } from "./pages/posts/create-post/create-post.component";
+import { EditPostComponent } from "./pages/posts/edit-post/edit-post.component";
+import { AuthGuard } from "./guard/auth.guard";
 
-// route guard
-// import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'home'
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-  },
-  {
-    path: 'posts',
-    component: PostsComponent ,
-  },
-  {
-    path: 'create-post',
-    component: CreatePostComponent ,
-  },
-  {
-    path: 'posts/update-post/:id',
-    component: EditPostComponent ,
-  },
-  {
-    path: 'posts/card',
-    component: CardComponent,
-  },
-  {
-    path: 'sign-in',
-    component: SignInComponent
-  },
-  { path: 'register-user', 
-  component: SignUpComponent 
-  },
-  {
-    path: '**',
-    component: PageNotFoundComponent
-  }
+    {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "home",
+    },
+    {
+        path: "home",
+        component: HomeComponent,
+    },
+    {
+        path: "posts",
+        component: PostsComponent,
+        canActivate: [AuthGuard],
+    },
+    {
+        path: "create-post",
+        component: CreatePostComponent,
+        canActivate: [AuthGuard],
+    },
+    {
+        path: "posts/update-post/:id",
+        component: EditPostComponent,
+        canActivate: [AuthGuard],
+    },
+    {
+        path: "posts/card",
+        component: CardComponent,
+        canActivate: [AuthGuard],
+    },
+    {
+        path: "sign-in",
+        component: SignInComponent,
+    },
+    { path: "register-user", component: SignUpComponent },
+    {
+        path: "**",
+        component: PageNotFoundComponent,
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- mySharedBlog/src/app/guard/auth.guard.ts
+++ mySharedBlog/src/app/guard/auth.guard.ts
@@ -1,27 +1,26 @@
-// import { Injectable } from '@angular/core';
-// import {
-//   CanActivate,
-//   ActivatedRouteSnapshot,
-//   RouterStateSnapshot,
-//   Router,
-// } from '@angular/router';
-// import { AuthService } from '../services/auth.service';
-// import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import {
+    CanActivate,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot,
+    Router,
+} from "@angular/router";
+import { AuthService } from "../services/auth.service";
+import { Observable } from "rxjs";
 
-// @Injectable({
-//   providedIn: 'root',
-// })
+@Injectable({
+    providedIn: "root",
+})
+export class AuthGuard implements CanActivate {
+    constructor(public authService: AuthService, public router: Router) {}
 
-// export class AuthGuard implements CanActivate {
-//   constructor(public authService: AuthService, public router: Router) {}
-
-//   canActivate(
-//     next: ActivatedRouteSnapshot,
-//     state: RouterStateSnapshot
-//   ): Observable<boolean> | Promise<boolean> | boolean {
-//     if (this.authService.isLoggedIn !== true) {
-//       this.router.navigate(['home']);
-//     }
-//     return true;
-//   }
-// }
+    canActivate(
+        next: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ): Observable<boolean> | Promise<boolean> | boolean {
+        if (this.authService.isLoggedIn !== true) {
+            this.router.navigate(["home"]);
+        }
+        return true;
+    }
+}
