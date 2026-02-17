106e8c7ec4fdf002845f0e48ac57b474a5ee74ad
NikolayMantchev <62190441+NikolayMantchev@users.noreply.github.com>
2026-02-17 22:25:43 +0100

    update firebase config

--- mySharedBlog/src/environments/environment.prod.ts
+++ mySharedBlog/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDrP6VdNgbBxgjXdSUxKySAYcHGiaCw0P0",
+    authDomain: "bloger-fa49f.firebaseapp.com",
+    databaseURL: "https://bloger-fa49f-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "bloger-fa49f",
+    storageBucket: "bloger-fa49f.appspot.com",
+    messagingSenderId: "642232990218",
+    appId: "1:642232990218:web:c5ca4b228606e9d9767df3"
+  }
 };
